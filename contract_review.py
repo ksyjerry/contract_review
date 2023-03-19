49f5f722959f69396fdccdb2de3d1b4595058710
ksyjerry <67769150+ksyjerry@users.noreply.github.com>
2023-03-19 14:04:00 +0900

    Update contract_review.py

--- contract_review.py
+++ contract_review.py
@@ -24,7 +24,7 @@ def get_text():
 
 def chatGPT_conversation(conversation):
     response = openai.ChatCompletion.create(
-            model = 'gpt-3.5-turbo',
+            model = 'gpt-4',
             messages = conversation
             
     )
