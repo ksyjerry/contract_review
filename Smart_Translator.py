97096a0b2dd40a06cf7fdf3f1c0381eb10c67ecc
ksyjerry <67769150+ksyjerry@users.noreply.github.com>
2023-03-29 22:09:12 +0900

    Update Smart_Translator.py

--- Smart_Translator.py
+++ Smart_Translator.py
@@ -10,7 +10,7 @@ openai.api_key = st.secrets["api_secret"]
 st.title('Assurance DA')
 st.header('AI Game Changer - Smart Translator')
 
-st.markdown("<div style='text-align: right;'>Developed by Assurance DA (문의 : <a href = \"mailto:jae-dong.kim@pwc.com\">jae-dong.kim@pwc.com</a>)</div>", unsafe_allow_html=True)
+# st.markdown("<div style='text-align: right;'>Developed by Assurance DA (문의 : <a href = \"mailto:jae-dong.kim@pwc.com\">jae-dong.kim@pwc.com</a>)</div>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 
 
